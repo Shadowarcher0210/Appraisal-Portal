68b25a631822b43c760aec86883da991d21f429e
venkatesh <venkatesh.ganaparapu@thebluespire.com>
2024-11-06 15:17:58 +0530

    Performance

--- Backend/routes/manager/PerformanceRoutes.js
+++ Backend/routes/manager/PerformanceRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getEmployeeAppraisals } = require('../../controllers/manager/Performance');
+const { getEmployeeAppraisals, evaluateAppraisalForm } = require('../../controllers/manager/Performance');
 const router = express.Router()
 router.get('/allAppraisals/:managerName/:startDate/:endDate', getEmployeeAppraisals)
 module.exports = router;
